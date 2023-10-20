cff3c58520105c22ecea7578a1f895b288700eec
Pedro <pedrosalviano170@gmail.com>
2023-10-20 09:41:25 -0300

    Untested-v0.2
    - Add init function to initialize modules
    - Add countTime variable
    - Add loops for delay after command

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ videoInput = cv2.VideoCapture('./sources/video.mp4')
 videoInput2 = cv2.VideoCapture('./sources/video2.mp4')
 counter = 0
 counter2 = 0
+countTime = 0
 
 portList = ['COM4', 'COM3', 'COM1', 'COM2']
 
-vd = VehicleDetector()
-try:
-    ser = serial.Serial('COM4', 9600, timeout=1)
-except:
-    print("Nao foi possível conectar ao Arduino.")
-    sys.exit()
+def init():
+    try:
+        vd = VehicleDetector()
+    except:
+        print("Nao foi possivel carregar o modulo de Deteccao.")
+        sys.exit()
+    try:
+        ser = serial.Serial('COM4', 9600, timeout=1)
+    except:
+        print("Nao foi possível conectar ao Arduino.")
+        sys.exit()
 
 #Funcao depreciada utilizada para inicialização da variável ser
 """def initializeSerial():
@@ -47,14 +53,29 @@ def findCars():
         #ser.write(f'{vehicle_count2}'.encode())
         if (vehicle_count>vehicle_count2):
             ser.write(b"G1R2")
+            for i in range(0, 12):
+                time.sleep(1)
+                countTime += 1
         elif (vehicle_count2>vehicle_count):
             ser.write(b"R1G2")
+            for i in range(0, 12):
+                time.sleep(1)
+                countTime += 1
         elif (vehicle_count==0):
             ser.write(b"R1G2")
+            for i in range(0, 12):
+                time.sleep(1)
+                countTime += 1
         elif (vehicle_count2==0):
             ser.write(b"G1R2")
+            for i in range(0, 12):
+                time.sleep(1)
+                countTime += 1
         elif (vehicle_count==0 and vehicle_count2==0):
             ser.write(b"R1R2")
+            for i in range(0, 12):
+                time.sleep(1)
+                countTime += 1
         for box in vehicle_boxes:
             x, y, w, h = box
             cv2.rectangle(source, (x, y), (x+w, y+h), (25, 0, 180), 3)
@@ -69,4 +90,5 @@ def findCars():
         if (cv2.waitKey(1) == 13):
             break
         time.sleep(1)
+
 findCars()
