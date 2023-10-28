dd3051336ec9dc183f24a5d81af6a42590db38f5
Pedro <pedrosalviano170@gmail.com>
2023-10-28 00:10:08 -0300

    Initial commit v0.3

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ import serial
 import sys
 import cvlib
 from urllib.request import urlopen
+import matplotlib.pyplot as plt
 
+# IF NEEDED CHANGE THE LOCAL IP ADDRESS
 URL = "http://192.168.0.4/capture"
 URL2 = "http://192.168.0.250/capture"
 
 #videoInput = cv2.VideoCapture(URL)
 #videoInput2 = cv2.VideoCapture(URL2)
+
 counter = 0
 counter2 = 0
 countTimeGreen1 = 0
@@ -22,7 +25,13 @@ countTimeRed1 = 0
 countTimeGreen2 = 0
 countTimeRed2 = 0
 
+countCars = 0
+sumTime = 0
+
 portList = ['COM4', 'COM3', 'COM1', 'COM2']
+counts = ['Green 1', 'Red 1', 'Green 2', 'Red 2']
+listCountCars = []
+listTime = []
 
 class VideoCapture:
     def __init__(self, name):
@@ -49,11 +58,16 @@ class VideoCapture:
 
 
 def init():
-    """ try:
-        videoInput = cv2.VideoCapture(URL)
+    try:
+        videoInput = urlopen(URL)
     except:
-        print("Nao foi possivel conectar a camera")
-        sys.exit() """
+        print("Nao foi possivel conectar a camera 1")
+        sys.exit()
+    try:
+        videoInput2 = urlopen(URL2)
+    except:
+        print("Nao foi possivel conectar a camera 2")
+        sys.exit()
     try:
         vd = VehicleDetector()
     except:
@@ -87,7 +101,7 @@ def captureFrame(input):
     ret, frame = input.read()
     return frame
 
-def findCars():
+def findCars(countTimeGreen1, countTimeGreen2, countTimeRed1, countTimeRed2, countCars, sumTime):
     while True:
         #source = captureFrame(videoInput)
         #source2 = captureFrame(videoInput2)
@@ -99,39 +113,47 @@ def findCars():
         print(f"Cruzamento 1: {vehicle_count}")
         print(f"Cruzamento 2: {vehicle_count2}")
         print("---")
+        countCars = countCars + (len(vehicle_boxes)+len(vehicle_boxes2))
+        listCountCars.append(countCars)
         #ser = initializeSerial()
         #ser.write(f'{vehicle_count}'.encode())
         #ser.write(f'{vehicle_count2}'.encode())
         if (vehicle_count>vehicle_count2):
             ser.write(b"G1R2")
-            """ for i in range(0, 12):
+            for i in range(0, 12):
                 time.sleep(1)
-                countTimeGreen1 += 1
-                countTimeRed2 += 1 """
+                sumTime += 1
+                countTimeGreen1 = countTimeGreen1 + 1
+                countTimeRed2 = countTimeRed2 + 1
         elif (vehicle_count2>vehicle_count):
             ser.write(b"R1G2")
-            """ for i in range(0, 12):
+            for i in range(0, 12):
                 time.sleep(1)
-                countTimeRed1 += 1
-                countTimeGreen2 += 1 """
+                sumTime += 1
+                countTimeRed1 = countTimeRed1 + 1
+                countTimeGreen2 = countTimeGreen2 + 1
         elif (vehicle_count==0):
             ser.write(b"R1G2")
-            """ for i in range(0, 12):
+            for i in range(0, 12):
                 time.sleep(1)
-                countTimeRed1 += 1
-                countTimeGreen2 += 1 """
+                sumTime += 1
+                countTimeRed1 = countTimeRed1 + 1
+                countTimeGreen2 = countTimeGreen2 + 1
         elif (vehicle_count2==0):
             ser.write(b"G1R2")
-            """ for i in range(0, 12):
+            for i in range(0, 12):
                 time.sleep(1)
-                countTimeGreen1 += 1
-                countTimeRed2 += 1  """
+                sumTime += 1
+                countTimeGreen1 = countTimeGreen1 + 1
+                countTimeRed2 = countTimeRed2 + 1
         elif (vehicle_count==0 and vehicle_count2==0):
             ser.write(b"R1R2")
-            """ for i in range(0, 12):
+            for i in range(0, 12):
                 time.sleep(1)
-                countTimeRed1 += 1
-                countTimeRed2 += 1 """
+                sumTime += 1
+                countTimeRed1 = countTimeRed1 + 1
+                countTimeRed2 = countTimeRed2 + 1
+        listTime.append(sumTime)
         for box in vehicle_boxes:
             x, y, w, h = box
             #cv2.rectangle(source, (x, y), (x+w, y+h), (25, 0, 180), 3)
@@ -143,8 +165,19 @@ def findCars():
         cv2.imshow('Source', source)
         #cv2.moveWindow('Source', 0,0)
         cv2.imshow('Source 2', source2)
-        if (cv2.waitKey(1) == 13):
-            break
+        if (cv2.waitKey(1) & 0xFF == ord('x')):
+            return countTimeGreen1, countTimeGreen2, countTimeRed1, countTimeRed2, countCars
+
+def plotting():
+    fig, ax = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    values = [countTimeGreen1, countTimeRed1, countTimeGreen2, countTimeRed2]
+    ax.bar(counts, values)
+    ax2.plot(listCountCars, listTime)
+    print(listCountCars)
+    print(listTime)
+    plt.show()
 
 vd, ser = init()
-findCars()
+countTimeGreen1, countTimeGreen2, countTimeRed1, countTimeRed2, countCars = findCars(countTimeGreen1, countTimeGreen2, countTimeRed1, countTimeRed2, countCars, sumTime)
+plotting()
